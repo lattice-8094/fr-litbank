f1c2edf666383ec461da10f98f3c0db83519f93b
Marco Naguib <marco@marco.naguib>
2022-01-21 14:05:15 +0100

    Revert "BIOES + pipeline propre"
    This reverts commit 57fbacf3f5b80afb9f2331d59ea6ccbccdfc1120.

--- src/brat2tsv.py
+++ src/brat2tsv.py
@@ -7,7 +7,6 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    #prendre les arguments
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-i', '--input', help='input directory containing brat files', required=True)
@@ -18,14 +17,10 @@ if __name__ == '__main__':
     inputDir = args["input"]
     outputDir = args["output"]
 
-    #créer le dossier de sortie si inexistant
     if not os.path.isdir(outputDir):
         os.mkdir(outputDir)
 
-    #parcourir les fichier brat un par un
     for filename in glob.glob(os.path.join(inputDir, '*.ann')):
-        # initialiser df, contenant les entités
-        # et text, contenant le text brut
         print(filename)
         with open(filename) as f:
             content = [l.split('\t')[1].split(' ') for l in f]
@@ -34,42 +29,25 @@ if __name__ == '__main__':
             df.nd = pd.to_numeric(df.nd)
             df.sort_values(['st','nd'], ascending=(True,False), inplace=True)
         with open(filename[:-3]+"txt") as f:
-            text = f.read().replace('’','\'')
+            text = f.read()
 
-        #le but est d'initialiser, à partir du texte brut, un dictionnaire words
-        #qui contient autant d'éléments que de mots dans le texte.
-        #un élément de ce dictionnaire a comme clé l'indice de début du mot et comme valeur,
-        #le dictionnaire {'next': l'indice du mot suivant,'text':le texte du mot,
-        #'entities': liste contenant les entités qui contiennent ce mot (cette liste
-        #est vide dans un premier temps)}
-        #
-        #Remarque: on sépare les "mots" sur les espaces, mais aussi sur les apostrophes,
-        #les points, les virgules, et les parenthèses.
         idx = 0
-        word_start_idx = 0
+        idx_st = 0
         words = {}
-        #parcourir les caractères du texte
         while idx<=len(text):
-            c = text[idx] if idx<len(text) else '.' #pour être sûr que la dernière phrase est terminée par un point
-            #on ne fait rien jusqu'à ce qu'on rencontre un séparateur de mot
-            #si c est un séparateur de mot :
-            if c in ['\'', '.', ',','\n', '(', ')','…','–','’','[',']','-', ' ']:
-                #cas particulier si c est un séparateur qui vient collé au caractère précédent, et qu'il faut donc séparer
-                if c in ['.', ',', ')',']','-']:
-                    words[word_start_idx] = {'next':idx, 'text':text[word_start_idx:idx], 'entities':[]}
-                    word_start_idx = idx
-                    idx+=1 if idx+1< len(text) and text[idx+1] == ' ' else 0 #décaler idx sauf s'il après un tiret, comme le mot suivant est collé
-                #c est un séparateur, donc on coupe le mot. on ajoute le mot qu'on a jusque là
-                #puis on redéfinit le word_start_idx pour que ce soit le idx actuel
-                words[word_start_idx] = {'next':idx+1, 'text':text[word_start_idx:idx+1], 'entities':[]}
-                word_start_idx = idx+1
+            c = text[idx] if idx<len(text) else '.'
+            if c in [' ','\'', '.', ',','\n', '(', ')','…','–','’','[',']']:
+                if c in ['.', ',', ')',']']:
+                    words[idx_st] = {'this': idx_st, 'next':idx, 'text':text[idx_st:idx], 'entities':[]}
+                    idx_st = idx
+                    idx+=1
+                words[idx_st] = {'this': idx_st, 'next':idx+1, 'text':text[idx_st:idx+1], 'entities':[]}
+                idx_st = idx+1
             idx+=1
-
-        #Maintenant, le but est de remplir les listes 'entities' de chaque mot, la liste
-        #des étiquettes (du type "B-PER"...) qui seront à côté de chaque mot
         max_depth=0
         df.index = range(len(df))
         df['lvl'] = [0]*len(df)
+
         for i, ent in df.iterrows():
             for level_ctr in range(1, min(5, len(df)-i)):
                 this_ent = (next_ent if level_ctr>1 else ent).copy()
@@ -81,36 +59,30 @@ if __name__ == '__main__':
                     break
             w = words[ent.st]
             l = len(w['entities'])
-            if w['next'] != ent.nd:
-                w['entities'].append(('B-'+ent.ct,ent.lvl))
-            else:
-                w['entities'].append(('S-'+ent.ct,ent.lvl))
-            while w['next'] <= len(text) and words[w['next']]['next'] < ent.nd:
+            w['entities'].append(('B-'+ent.ct,ent.lvl))
+            while w['next'] <= len(text) and w['next'] < ent.nd:
                 w = words[w['next']]
                 while len(w['entities'])!=l:
                     assert len(w['entities'])<l
                     w['entities'].append(('O',l-len(w['entities'])))
                 w['entities'].append(('I-'+ent.ct,ent.lvl))
             w = words[w['next']]
-            if w['next'] <= ent.nd:
-                w['entities'].append(('E-'+ent.ct,ent.lvl))
-            w = words[w['next']]
 
         output = ''
         contained_first = True
-        for i,w in enumerate(list(words.values())):
+        for w in words.values():
             sent_end=False
             if w['text']=='\n' or w['text']=='':
                 continue
-            output+=w['text'].replace('\n','').replace(' ','')
-            if w['text'][0]=='.' and list(words.values())[i-1]['text'] not in ['M','MM','Mm','L']:
+            output+=''+w['text'].replace('\n','').replace(' ','')+' '
+            if w['text'][0]=='.':
                 sent_end= True
             ents = {k:v for v,k in w['entities']}
             for i in range(max_depth):
                 if contained_first:
-                    output+= '\t{}'.format(ents[i] if i in ents.keys() else 'O')
+                    output+= '{}\t'.format(ents[i] if i in ents.keys() else 'O')
                 else :
-                    output+= '\t{}'.format(w['entities'][i][0] if i<len(w['entities']) else 'O')
+                    output+= '{}\t'.format(w['entities'][i][0] if i<len(w['entities']) else 'O')
             output+='\n'
             if sent_end:
                 output+='\n'
